ccad3a9b7bf8a33c8d5c03d4f49f85054e0e1ba4
Pugazhezhilan <pugazhsumathi007@gmail.com>
2026-03-28 21:24:16 +0530

    implementing enimies

--- classes/Platform.js
+++ classes/Platform.js
@@ -20,3 +20,53 @@ class Platform {
     )
   }
 }
+
+class Enemy{
+  constructor({x,y,width=16,height=16}){
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+
+    this.velocity = {
+      x: 40,
+      y: 0
+    }
+
+    this.gravity = 500;
+    this.isOnGrow = false;
+  }
+
+  getBounds(){
+    return{
+      x: this.x,
+      y: this.y,
+      width: this.width,
+      height: this.height
+    }
+  }
+
+  draw(c){
+    c.fillStyle = "purple" // enemy color lol
+    c.fillRect(this.x, this.y, this.width, this.height)
+  }
+
+  update(deltaTime, collisionBlocks){
+    // apply gravity
+    this.velocity.y += this.gravity*deltaTime;
+
+    // move X
+    this.x += this.velocity.x*deltaTime
+
+    for(let i=0;i<collisionBlocks.length;i++){
+      const block = collisionBlocks[i];
+
+      if(this.x < block.width && this.x + this.width > block.x && this.y + this.height > block.y && this.y < block.y + block.height){
+        // reverse direction
+        this.velocity.x = -this.velocity.x
+
+        
+      }
+    }
+  }
+}
